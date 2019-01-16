650c35b2d5f1de92540a264d375caf2a6333d0b7
sebastianCao <42419360+sebastianCao@users.noreply.github.com>
2019-01-15 21:18:18 -0800

    Create Exercise52.rkt

---
+++ Ch1 Fixed Size Data/Exercise52.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise52) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; [3, 5] includes 3 4 5 
+; (3, 5] includes 4 5
+; [3, 5) includes 3 4
+; (3, 5) includes 4
+
