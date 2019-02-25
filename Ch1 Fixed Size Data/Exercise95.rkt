b46b8bcad89ab1b8d218f8d68195793979566dc5
sebastianCao <42419360+sebastianCao@users.noreply.github.com>
2019-02-24 18:47:14 -0800

    Create Exercise95.rkt

---
+++ Ch1 Fixed Size Data/Exercise95.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise95) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; The first instance corresponds to the first clause of the data definition, (make-aim ufo tank)
+; The second and third instances correspond to the second clause of the data definition, (make-fired ufo tank missile)
